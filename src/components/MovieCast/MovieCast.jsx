8285fbb35f88a95fda366d3f177198a7ebcced47
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2024-09-29 13:53:43 +0200

    fix

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -5,18 +5,18 @@ import css from '../MovieCast/MovieCast.module.css';
 export default function MovieCast() {
 	const { cast } = useMovie();
 
-	const castWithPhoto = cast.filter(c => c.profile_path !== null);
+	const castWithPhoto = cast.filter(cast => cast.profile_path !== null);
 
 	return (
 		<div className={css.cast}>
 			{castWithPhoto.length > 0 ? (
-				castWithPhoto.slice(0, 20).map(c => (
-					<div className={css.cast_card} key={c.id}>
+				castWithPhoto.slice(0, 20).map(cast => (
+					<div className={css.cast_card} key={cast.id}>
 						<div className={css.cast_image}>
-							<img src={`${imageBaseURL}${c.profile_path}`} alt={`${c.name} photo`} />
+							<img src={`${imageBaseURL}${cast.profile_path}`} alt={`${cast.name} photo`} />
 						</div>
-						<h3>{c.name}</h3>
-						<p>as {c.character}</p>
+						<h3>{cast.name}</h3>
+						<p>as {cast.character}</p>
 					</div>
 				))
 			) : (
